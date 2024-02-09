67e9b9472ea0077b6fb71dda9297ade8244bfcfe
badalsharma138 <007sharmakumarbadal@gmail.com>
2024-02-09 19:45:48 +0530

    project complete

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback,useEffect,useRef } from 'react'
 
 
 function App() {
@@ -6,12 +6,17 @@ function App() {
   const [number,setNumber]=useState(false)
   const [char,setChar]=useState(false)
   const [password,setPassword]=useState(false)
+  const [isClicked, setIsClicked] = useState(false);
+
+
+
+  const passwordRef=useRef(null);
   
   const passwordGenerator=useCallback(()=>{
     let pass=""
-    let str="A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R,a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, and"
+    let str="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
     if(number){
-     str+="11234567890"
+     str+="1234567890"
     }
     if(char){
       str+="!@#$%^&*())_"
@@ -23,7 +28,18 @@ function App() {
     setPassword(pass)
   },[length,number,char,setPassword])
 
+  const copyPasswordToClipBoard=useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+    setIsClicked(true);
 
+    // Reset the click effect after a certain delay
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 200); // Adjust the delay as needed
+  },[password])
+
+ useEffect(()=>{passwordGenerator()},[length,number,char,passwordGenerator])
   return (
     <>
      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-1 my-8 text-orange-500 bg-gray-700'>
@@ -34,8 +50,12 @@ function App() {
       className='outline-none w-full py-1 px-3'
       placeholder='Password'
       readOnly
+      ref={passwordRef}
       />
-      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+      <button  onClick={copyPasswordToClipBoard}
+            className={`outline-none px-3 py-0.5 ${
+              isClicked ? 'bg-grey-700 text-white' : 'bg-blue-700 text-white'
+            }`}>
         copy
       </button>
       </div>
@@ -50,7 +70,28 @@ function App() {
           <label >Length:{length}</label>
           
         </div>
-        
+        <div className=' flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={number}
+          id='numberInput'
+          onChange={()=>{
+            setNumber((prev)=>!prev);
+          }}
+           />
+           <label htmlFor='number'>Number</label>
+           
+        </div>
+        <div className=' flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={char}
+          id='charInput'
+          onChange={()=>{
+            setChar((prev)=>!prev);
+          }}
+           />
+           <label htmlFor='number'>Char</label>
+           
+        </div>
       </div>
      </div>
     </>
